d695ece2ddeb11577a8797858550a53a81183e24
bruno-gsousa <bruno.gsousa.senac@gmail.com>
2026-07-06 20:05:34 -0300

    fundamentos_django

---
+++ meusite/meusite/view.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+
+def teste_view(request):
+    return HttpResponse("<h1>Olá, mundo! Esta é uma view de teste.</h1>")
+
+def index_view(request):
+    return HttpResponse("<h1>Bem-vindo à página inicial!</h1><p style='color: blue;'>Agora meu sistema está em desenvolvimento.</p>")
